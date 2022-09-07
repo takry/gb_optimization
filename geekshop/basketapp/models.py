036e82177c7255212947b8acc61b07534a632f92
Nikolay Nikitin <takry17@gmail.com>
2022-09-07 15:31:40 +0300

    Lesson_6

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 
 from mainapp.models import Product
+from django.utils.functional import cached_property
 
 
 class BasketQuerySet(models.QuerySet):
@@ -28,19 +29,23 @@ class Basket(models.Model):
 
     add_datetime = models.DateTimeField(verbose_name='время', auto_now_add=True)
 
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
+
     @property
     def product_cost(self):
         return self.product.price * self.quantity
 
     @property
     def total_quantity(self):
-        _items = Basket.objects.filter(user=self.user)
+        _items = self.get_items_cached
         _total_quantity = sum(list(map(lambda x: x.quantity, _items)))
         return _total_quantity
 
     @property
     def total_cost(self):
-        _items = Basket.objects.filter(user=self.user)
+        _items = self.get_items_cached
         _total_cost = sum(list(map(lambda x: x.product_cost, _items)))
         return _total_cost
 
